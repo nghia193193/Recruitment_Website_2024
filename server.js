309d6acf95fbf9ac790bc2263d94ede89a901241
Nghĩa Nguyễn <nguyennghia193913@gmail.com>
2024-07-09 12:45:44 +0000

    Update 62 files
    - /src/api/v1/auth/checkAuth.js
    - /src/api/v1/configs/config.coudinary.js
    - /src/api/v1/configs/config.fileUpload.js
    - /src/api/v1/configs/config.mongodb.js
    - /src/api/v1/controllers/access.controller.js
    - /src/api/v1/controllers/admin.controller.js
    - /src/api/v1/controllers/recruiter.controller.js
    - /src/api/v1/core/error.response.js
    - /src/api/v1/core/reasonPhrases.js
    - /src/api/v1/core/statusCodes.js
    - /src/api/v1/core/success.response.js
    - /src/api/v1/dbs/init.mongodb.js
    - /src/api/v1/dbs/init.redis.js
    - /src/api/v1/helpers/check.connect.js
    - /src/api/v1/middlewares/index.js
    - /src/api/v1/models/admin.model.js
    - /src/api/v1/models/application.model.js
    - /src/api/v1/models/candidate.model.js
    - /src/api/v1/models/favoriteJob.model.js
    - /src/api/v1/models/favoriteRecruiter.model.js
    - /src/api/v1/models/job.model.js
    - /src/api/v1/models/login.model.js
    - /src/api/v1/models/otp.model.js
    - /src/api/v1/models/recruiter.model.js
    - /src/api/v1/models/resume.model.js
    - /src/api/v1/postman/access.post.http
    - /src/api/v1/postman/admin.http
    - /src/api/v1/postman/recruiter.http
    - /src/api/v1/services/access.service.js
    - /src/api/v1/services/admin.service.js
    - /src/api/v1/services/jwt.service.js
    - /src/api/v1/services/otp.service.js
    - /src/api/v1/services/recruiter.service.js
    - /src/api/v1/services/redis.service.js
    - /src/api/v1/utils/findUser.js
    - /src/api/v1/utils/index.js
    - /src/api/v1/utils/logEvents.js
    - /src/api/v1/utils/sendMails.js
    - /src/api/v1/validations/access.validation.js
    - /src/api/v1/validations/admin.validation.js
    - /src/api/v1/validations/job.validation.js
    - /src/api/v1/validations/recruiter.validation.js
    - /src/api/v1/logs/error_logs/log.11_04_2024
    - /src/api/v1/logs/error_logs/log.12_04_2024
    - /src/api/v1/logs/error_logs/log.14_04_2024
    - /src/api/v1/logs/error_logs/log.15_04_2024
    - /src/api/v1/logs/error_logs/log.16_04_2024
    - /src/api/v1/logs/error_logs/log.17_04_2024
    - /src/api/v1/logs/error_logs/log.18_04_2024
    - /src/api/v1/routes/access/index.js
    - /src/api/v1/routes/admin/index.js
    - /src/api/v1/routes/recruiter/index.js
    - /src/api/v1/logs/log.07_04_2024
    - /src/api/v1/logs/log.08_04_2024
    - /src/api/v1/logs/log.09_04_2024
    - /src/api/v1/logs/log.10_04_2024
    - /src/api/v1/routes/index.js
    - /src/api/v1/app.js
    - /.gitignore
    - /package-lock.json
    - /package.json
    - /server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/api/v1/app');
-const { app: { port } } = require('./src/api/v1/configs/config.mongodb')
-
-const server = app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
-
